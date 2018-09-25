5155ffe4554fa9043456043a889b6d9784e54fa3
Leif Hetland <lehetland1@gmail.com>
2018-09-25 16:09:12 -0400

    Added tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [
 		text: 'First test todo'
 	}, {
 		_id: new ObjectID(),
-		text: 'Second test todo'
+		text: 'Second test todo',
+		completed: true,
+		completedAt: 333
 	}
 ];
 
@@ -150,3 +152,37 @@ describe('DELETE /todos/:id', () => {
 	});
 
 });
+
+describe('PATCH /todos/:id', () => {
+	it('should update the todo', (done) => {
+		let hexId = todos[0]._id.toHexString();
+		let text = 'new text';
+
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({text, completed: true})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(true);
+				expect(res.body.todo.completedAt).toBeA('number');
+			})
+			.end(done);
+	});
+
+	it('should clear completedAt when todo is not completed', (done) => {
+		let hexId = todos[0]._id.toHexString();
+		let text = 'new text';
+
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({text: text, completed: false})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.completed).toBe(false);
+				expect(res.body.todo.completedAt).toBeNull;
+			})
+			.end(done);
+	});
+}); 
